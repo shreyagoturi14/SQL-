c58e741e76b383419e206329b97bf62c283a76b6
Shreya G <shreyagoturi@gmail.com>
2026-07-04 23:06:29 +0530

    Update 584_Find_Customer_Referee.sql

--- LeetCode_SQL/584_Find_Customer_Referee.sql
+++ LeetCode_SQL/584_Find_Customer_Referee.sql
@@ -1,1 +1,12 @@
+-- LeetCode 584: Find Customer Referee
 
+-- Concepts Used:
+-- SELECT
+-- WHERE
+-- NULL Handling (IS NULL)
+-- Logical Operator (OR)
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+   OR referee_id IS NULL;
