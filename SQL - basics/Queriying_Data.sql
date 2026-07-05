74599ff1d8625796c5c5417656e1ed866a09ce76
Shreya G <shreyagoturi@gmail.com>
2026-07-05 11:26:41 +0530

    Add files via upload

---
+++ SQL - basics/Queriying_Data.sql
@@ -0,0 +1,14 @@
+USE startersql;
+-- SELECT * from users WHERE date_of_birth<'1995-09-09';
+
+-- SELECT * from users WHERE id>=10;
+
+-- SELECT * from users WHERE date_of_birth BETWEEN '1995-09-09' AND '1999-09-09';
+
+-- SELECT * from users WHERE gender in ('Male','Female');
+
+-- SELECT * from users WHERE gender='Female' AND salary>'70000';
+
+-- SELECT * from users WHERE gender='Male' OR salary>'65000' ORDER BY date_of_birth DESC;
+
+SELECT * from users WHERE gender='Male' OR salary>'65000' ORDER BY date_of_birth DESC LIMIT 5;
